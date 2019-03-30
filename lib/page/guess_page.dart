695b37c66dce38c2b6dbd19157f93b3c908ec646
Caleb Young <caleb.a.young@gmail.com>
2019-03-29 20:36:30 -0600

    Set up containers for guess page.

--- lib/page/guess_page.dart
+++ lib/page/guess_page.dart
@@ -11,7 +11,26 @@ class GuessPageState extends State<GuessPage>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        title: Text('Evil Hangman'),
+      ),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget> [
+            Container (
+              color: Colors.green,
+              height: 150.0
+            ),
+            Expanded (
+              child: Container (
+                color: Colors.red
+              )
+            ),
+          ]
+        )
+      )
     );
   }
 }
