81c4a87cf0800423b9196a1c84aa9e6fcbd4c8a2
Caleb Young <caleb.a.young@gmail.com>
2019-03-29 20:23:58 -0600

    Set up load dictionary function.

--- lib/page/start_page.dart
+++ lib/page/start_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:evil_hangman/controller/word_controller.dart';
+import 'package:evil_hangman/page/guess_page.dart';
 
 class StartPage extends StatefulWidget {
   @override
@@ -8,6 +10,8 @@ class StartPage extends StatefulWidget {
 }
 
 class StartPageState extends State<StartPage>{
+  int _wordLength = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,18 +26,26 @@ class StartPageState extends State<StartPage>{
             new TextField(
               decoration: new InputDecoration(labelText: "Enter desired word size"),
               keyboardType: TextInputType.number,
+              onChanged: (value) {
+                this._wordLength = int.tryParse(value);
+              },
             ),
             Padding(
               padding: EdgeInsets.all(35.0),
               child: RaisedButton(
-                onPressed: () {},
+                onPressed: () => beginGame(context),
                 child: const Text('Begin a Game'),
               ),
             )
-
           ],
         )
       )
     );
+  }
+
+  Future beginGame(BuildContext context) async {
+    var wordController = WordController(); // TODO: call the loadDictionary function
+    await wordController.loadDictionary(this._wordLength);
+    Navigator.push(context, MaterialPageRoute(builder: (context) => GuessPage()));
   }
 }
