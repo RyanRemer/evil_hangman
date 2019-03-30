d1d9be4fe2ed29f6aeb39fff95a389af9e5366e2
RyanRemer <ryandremer@gmail.com>
2019-03-29 19:43:02 -0600

    loadDictionary Function Done

--- lib/controller/word_controller.dart
+++ lib/controller/word_controller.dart
@@ -1,5 +1,22 @@
+import 'dart:convert';
+
 import 'package:evil_hangman/data/client_model.dart';
+import 'package:evil_hangman/data/word.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 class WordController {
   ClientModel _clientModel = ClientModel.getInstance();
+
+  WordController();
+
+  Future loadDictionary(int wordLength) async {
+    String fileData = await rootBundle.loadString('assets/words.json');
+    var fileStrings = json.decode(fileData);
+    _clientModel.dictionary.clear();
+    for (var fileString in fileStrings){
+      if (fileString.length == wordLength){
+        _clientModel.dictionary.add(Word(fileString));
+      }
+    }
+  }
 }
--- lib/page/start_page.dart
+++ lib/page/start_page.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
 
-class StartPage extends StatefulWidget{
+class StartPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return StartPageState();
   }
 }
 
-class StartPageState extends State<StartPage>{
+class StartPageState extends State<StartPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
-    );
+    return Scaffold();
   }
 }
