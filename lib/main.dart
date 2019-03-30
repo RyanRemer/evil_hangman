2fdc17d3a31b291adb639bb399cef2a91dd4f74e
RyanRemer <ryandremer@gmail.com>
2019-03-30 13:19:28 -0600

    small fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:evil_hangman/page/start_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
