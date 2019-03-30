cf0884e2e27fa1f0fad3a0efe15aecfcf6a20184
RyanRemer <ryandremer@gmail.com>
2019-03-30 15:15:06 -0600

    show definition of word

--- lib/page/score_page.dart
+++ lib/page/score_page.dart
@@ -1,6 +1,8 @@
+import 'package:evil_hangman/controller/word_controller.dart';
 import 'package:evil_hangman/widget/hidden_word_view.dart';
 import 'package:evil_hangman/widget/score_view.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ScorePage extends StatefulWidget {
   @override
@@ -17,30 +19,44 @@ class ScorePageState extends State<ScorePage> {
         title: Text("Final Score"),
       ),
       body: Container(
-        child: Column(
+        child: ListView(
           children: <Widget>[
             HiddenWordView(),
-            Expanded(
-              child: Container(
-                  padding: EdgeInsets.all(32),
-                  child: Column(
-                    children: <Widget>[
-                      Expanded(
-                        child: ScoreView(),
-                      )
-                    ],
-                  )),
-            ),
             Container(
-                padding: EdgeInsets.all(64.0),
-                child: Center(
-                  child: RaisedButton(
-                      child: Text("Try Again"), onPressed: startNewGame),
-                ))
+              padding: EdgeInsets.all(32.0),
+              height: 300,
+              child: ScoreView(),
+            ),
+            Column(
+              children: <Widget>[
+                RaisedButton(
+                    child: Container(
+                      padding: EdgeInsets.all(16.0),
+                      child: Text("Try Again"),
+                    ), onPressed: startNewGame),
+                Padding(padding: EdgeInsets.only(top: 16),),
+                RaisedButton(
+                    child: Container(
+                      padding: EdgeInsets.all(16.0),
+                      child: Text("View Definition"),
+                    ), onPressed: showDefinition),
+              ],
+            )
           ],
         ),
       ),
     );
+  }
+
+  Future showDefinition() async {
+    var hiddenWord = WordController().getHiddenWord().toString();
+
+    var url = 'https://www.dictionary.com/browse/' + hiddenWord;
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 
   void startNewGame() {
